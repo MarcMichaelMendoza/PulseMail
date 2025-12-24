4c0bc4a6a9ff295d1faf5fd3abe35d6f187ab97b
MarcMichaelMendoza <mmarcmichael@gmail.com>
2025-12-25 01:37:45 +0800

    Refactor Google OAuth strategy configuration and enhance logging

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const keys = require('./config/key');
 
 const app = express();
 
+// Configure Passport to use Google OAuth strategy
 passport.use(new googleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
+}, (accessToken, refreshToken, profile, done) => {
     console.log('Access Token:', accessToken);
+    console.log('Refresh Token:', refreshToken);
+    console.log('Profile:', profile);
 }));
+
+// Route to initiate Google OAuth
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']  })); 
+
+// Callback route for Google OAuth
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);   
