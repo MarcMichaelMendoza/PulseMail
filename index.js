1c37ccecc23c0f058efd5663a3847d9e7e211a62
MarcMichaelMendoza <mmarcmichael@gmail.com>
2025-12-25 00:58:29 +0800

    Add passport and Google OAuth dependencies; update .gitignore

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/key');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there' });
-});
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log('Access Token:', accessToken);
+}));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);   
