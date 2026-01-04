bb8814c61270e0e1c5c9536ca17ce70040f009ae
MarcMichaelMendoza <mmarcmichael@gmail.com>
2026-01-04 23:19:46 +0800

    feat: Initialize React application with essential files and configurations
    - Added logo image (logo512.png) for the application.
    - Created manifest.json for PWA support with app metadata.
    - Included robots.txt to manage web crawler access.
    - Added App.css for styling the main application component.
    - Implemented App.js as the main component with a welcome message.
    - Created App.test.js for basic testing of the App component.
    - Added global styles in index.css.
    - Set up index.js to render the App component and report web vitals.
    - Included logo.svg for the application branding.
    - Implemented reportWebVitals.js for performance measurement.
    - Set up testing environment with setupTests.js for testing utilities.

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use(passport.session());
 // Set up authentication routes 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);   
