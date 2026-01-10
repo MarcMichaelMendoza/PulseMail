6dc237ef64480e2bef35b27fb39875948563cc23
MarcMichaelMendoza <mmarcmichael@gmail.com>
2026-01-10 23:31:09 +0800

    Fix fetchUser action to dispatch user data correctly and update authReducer to handle null state

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,12 @@
+import { FETCH_USER } from '../actions/type';
+
 // Auth reducer to manage authentication state
-export default function (state = {}, action) {
+export default function (state = null, action) {
      console.log(action);
      switch(action.type) {
-        default: 
-            return state;
+          case FETCH_USER:
+               return action.payload || false;
+          default: 
+               return state;
      }
 }
