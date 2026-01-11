e068838e406eb4560a51f1b7cea066763e3d084b
MarcMichaelMendoza <mmarcmichael@gmail.com>
2026-01-11 18:30:27 +0800

    Refactor Header component to conditionally render login/logout links based on auth state and rename authReducer function for clarity

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_USER } from '../actions/type';
 
 // Auth reducer to manage authentication state
-export default function (state = null, action) {
+export default function authReducer(state = null, action) {
      console.log(action);
      switch(action.type) {
           case FETCH_USER:
