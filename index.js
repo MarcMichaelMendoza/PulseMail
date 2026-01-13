7cd92e8d879ce6e0c04871e8eb2f6a3c50516f55
MarcMichaelMendoza <mmarcmichael@gmail.com>
2026-01-13 15:08:25 +0800

    ✨ fix: Update React routing to handle all requests correctly in production

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
     
     // Handle React routing, return all requests to React app
     const path = require('path');
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
